f2a8b6864d5fa59002b0b979af64b3c1d2dcbef1
ZeroC0D3 Team <zeroc0d3.0912@gmail.com>
2017-04-28 13:42:00 +0700

    Add dockerfile test & spec file

---
+++ TDD-Docker/spec/Dockerfile_spec.rb
@@ -0,0 +1,29 @@
+# spec/Dockerfile_spec.rb
+
+require "serverspec"
+require "docker"
+
+describe "Dockerfile" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  it "installs the right version of Ubuntu" do
+    expect(os_version).to include("Ubuntu 16")
+  end
+
+  # This test is new
+  #it "installs required packages" do
+  #  expect(package("nodejs")).to be_installed
+  #end
+
+  def os_version
+    command("lsb_release -a").stdout
+  end
+end
+
+
